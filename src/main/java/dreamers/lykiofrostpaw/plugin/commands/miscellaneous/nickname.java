a26613fdc67f00afd4ad779ed482a844e450b5f0
Lykió Gelursî <mwgin.2001@gmail.com>
2019-12-05 17:19:44 -0600

    time :clap: for :clap: raids :clap:

--- src/main/java/dreamers/lykiofrostpaw/plugin/commands/miscellaneous/nickname.java
+++ src/main/java/dreamers/lykiofrostpaw/plugin/commands/miscellaneous/nickname.java
@@ -1,9 +1,11 @@
 package dreamers.lykiofrostpaw.plugin.commands.miscellaneous;
 
 import dreamers.lykiofrostpaw.plugin.Telepowort;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 // IT BEGINS
 public class nickname implements CommandExecutor {
@@ -14,7 +16,22 @@ public class nickname implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender player, Command command, String label, String[] args) {
-        return false;
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can use this command.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if (args.length == 0) {
+            player.sendMessage(ChatColor.RED + "You didn't specify a nickname.");
+            return true;
+        }
+
+        String nickname = "";
+
+
+        return true;
     }
 }
